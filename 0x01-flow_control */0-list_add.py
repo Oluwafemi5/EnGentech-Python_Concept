64ac0e916f6b6842179c86365b0b35035bf736d0
James Okonita <118991124+Oluwafemi5@users.noreply.github.com>
2023-06-04 18:35:55 +0100

    0-list_add.py

--- 0x01-flow_control */0-list_add.py
+++ 0x01-flow_control */0-list_add.py
@@ -1,1 +1,29 @@
+Lst1 = [8, 3, 9, 5] 
 
+Lst2 = [2, 1, 4, 18]
+
+print('Lst1 =' , Lst1)
+
+print('Lst2 =' , Lst2)
+
+Lst2.pop(0)
+
+Lst1.extend(Lst2)
+
+print('Appended List =' ,Lst1)
+
+def list_add():
+
+ Lst1 = [8, 3, 9, 5] 
+
+ Lst2 = [2, 1, 4, 18]
+
+ sum1=sum(Lst1)
+
+ sum2=sum(Lst2)
+
+ Total= sum1 + sum2
+
+ print('Sum Appended ='  ,[Total]) 
+
+list_add()
